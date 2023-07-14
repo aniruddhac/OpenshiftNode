21ef252392c69d1e14e44bf2de2097f633c2e82c
Aniruddha Chakrabarti <caniruddha@hotmail.com>
2023-07-14 17:25:02 +0530

    Updated

--- NodeHelloWorld.js
+++ NodeHelloWorld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello World from Node.js deployed in OpenShift');
+  res.end('Hello World from Node.js deployed in OpenShift. Updated.');
 });
 server.listen(8080);
